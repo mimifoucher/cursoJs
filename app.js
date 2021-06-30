9c7dad9d35d18ff2d256be1e1eb38c9c99882baa
Michelle Foucher <michelle.foucher@gmail.com>
2021-06-29 21:45:13 -0300

    Entrega desafío tres

--- app.js
+++ app.js
@@ -7,18 +7,24 @@ console.log("Mi nombre es " + nombre); */
 //Segundo Desafio
 
 
-const pago = prompt ("Pagas con transferencia bancaria?");
+// const pago = prompt ("Pagas con transferencia bancaria?");
 
-if (pago == "si") {
-    alert ("Tenes un 10% de descuento en tu compra");
+// if (pago == "si") {
+//     alert ("Tenes un 10% de descuento en tu compra");
 
-    const total = prompt ("Cual es el monto total de la compra?");
-    if (parseInt(total) >= 3000) {
-        alert("Tenes otro 10% de descuento");
-    } else {
-        alert ("No tenes otro 10% de descuento");
-    }
-} else {
-    alert ("No obtenes descuento");
+//     const total = prompt ("Cual es el monto total de la compra?");
+//     if (parseInt(total) >= 3000) {
+//         alert("Tenes otro 10% de descuento");
+//     } else {
+//         alert ("No tenes otro 10% de descuento");
+//     }
+// } else {
+//     alert ("No obtenes descuento");
+// }
+
+
+//Tercer Desafío
+
+for (let i = 0; i <= 50; i += 5) {
+    console.log (i);
 }
-
