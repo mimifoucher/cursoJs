89b9e7e05c56821302b61b6e3f8e40abc8178118
Michelle Foucher <michelle.foucher@gmail.com>
2021-07-01 15:40:24 -0300

    Desafío 4, principal

--- app.js
+++ app.js
@@ -25,6 +25,21 @@ console.log("Mi nombre es " + nombre); */
 
 //Tercer Desafío
 
-for (let i = 0; i <= 50; i += 5) {
-    console.log (i);
+// for (let i = 0; i <= 50; i += 5) {
+//     console.log (i);
+// }
+
+
+//Cuarto Desafio
+
+function edadPromedio() {
+    const edad1=parseInt(prompt("Cual es tu edad?"));
+    const edad2=parseInt(prompt("Cual es tu edad?"));
+    const edad3=parseInt(prompt("Cual es tu edad?"));
+    const edad4=parseInt(prompt("Cual es tu edad?"));
+    const edad5=parseInt(prompt("Cual es tu edad?"));
+
+    alert(Math.round(edad1+edad2+edad3+edad4+edad5)/5);
 }
+
+edadPromedio();
