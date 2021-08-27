5e16df4344c5957f89eb27a3713fcd30dec5c248
Michelle Foucher <michelle.foucher@gmail.com>
2021-08-27 20:16:01 -0300

    Entrega desafio dos - crear Algoritmo

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 //Primer Desafio
 
-const nombre = prompt("Cual es tu nombre?");
+// const nombre = prompt("Cual es tu nombre?");
 
-console.log("Mi nombre es " + nombre); 
+// console.log("Mi nombre es " + nombre); 
 
-const edad = prompt("Cual es tu edad?");
+// const edad = prompt("Cual es tu edad?");
 
-const edadCumple= parseInt(edad)+1;
+// const edadCumple= parseInt(edad)+1;
 
-console.log("El proximo año tendrás " + edadCumple);
+// console.log("El proximo año tendrás " + edadCumple);
 
+
+//Segundo Desafio - Crear un Algoritmo con un condicional
+
+let edad = prompt("Cual es tu edad?");
+
+if(edad <= 18){
+    alert("No puede ingresar al sitio ya que es menor de edad");
+}else{
+    alert("Bienvenido al nuestro sitio");
+}
 
 
 
