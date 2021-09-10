9075e9b475d8058f99c46508c51142cc2ce3a1d5
Michelle Foucher <michelle.foucher@gmail.com>
2021-09-10 12:53:25 -0300

    Entrega desafío:Objetos y Arrays

--- app.js
+++ app.js
@@ -26,28 +26,56 @@
 
 //Este ejercicio calcula del 1 al 10
 
-let pedirUno = parseInt(prompt("Escribir número uno"));
+// let pedirUno = parseInt(prompt("Escribir número uno"));
 
-for( ;pedirUno <=10; pedirUno++){
-    console.log(pedirUno);
-}
+// for( ;pedirUno <=10; pedirUno++){
+//     console.log(pedirUno);
+// }
 
 
 //Simulador Interactivo
 //Este ejercicio realiza la suma de los productos seleccionados por el usuario y devuelve el total a pagar
 
-const productoUno = prompt("Ingrese el nombre del primer producto");
-const precioUno = parseInt(prompt("Ingrese el precio del primer producto")) ;
-const productoDos = prompt("Ingrese el nombre del segundo producto");
-const precioDos = parseInt(prompt("Ingrese el precio del segundo producto"));
+// const productoUno = prompt("Ingrese el nombre del primer producto");
+// const precioUno = parseInt(prompt("Ingrese el precio del primer producto")) ;
+// const productoDos = prompt("Ingrese el nombre del segundo producto");
+// const precioDos = parseInt(prompt("Ingrese el precio del segundo producto"));
 
-sumaProductos(precioUno,precioDos);
+// sumaProductos(precioUno,precioDos);
 
-function sumaProductos(precioPrimerProducto, precioSegundoProducto){
-    const precioTotal = precioPrimerProducto + precioSegundoProducto;
-    console.log(precioTotal);
+// function sumaProductos(precioPrimerProducto, precioSegundoProducto){
+//     const precioTotal = precioPrimerProducto + precioSegundoProducto;
+//     console.log(precioTotal);
+// }
+
+//Entrega Desafío Objetos y Arrays
+
+//Utilización de clases 
+class Producto{
+    constructor(nombre,cantidad,precio){
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+    productoSeleccionado(){
+        console.log("El producto " + this.nombre + " ha sido sumado al carrito de compras")
+    }
 }
 
+// Creación de objeto
+const producto1 = new Producto("Lentejas","1 kilo",300);
+const producto2 = new Producto("Porotos","500 gramos",150);
+const producto3 = new Producto("Garbanzos","1 kilo",320);
+producto2.productoSeleccionado();
 
+let carrito = [];
+//Utilización de algunos métodos Arrays
 
-
+//Agregar productos al carrito
+carrito.push(producto1);
+carrito.push(producto2);
+carrito.push(producto3);
+console.log(carrito);
+//Elimina un producto del carrito
+carrito.pop();
+console.log(carrito);
